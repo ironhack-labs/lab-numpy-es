4782e6380557a41c1ed50aaf2cf36bdfec6c65df
monicalopezcalero <lopezcaleromonica@gmail.com>
2024-11-03 22:11:23 +0100

    lab finalizado

--- your-code/main_esp.py
+++ your-code/main_esp.py
@@ -1,71 +1,91 @@
 #1. Importa el paquete NUMPY bajo el nombre np.
 
-#[tu código aquí]
+import numpy as np
 
 
 #2. Imprime la versión de NUMPY y la configuración.
 
-#[tu código aquí]
-
+print(np.version.version)
 
 #3. Genera un array tridimensional de 2x3x5 con valores aleatorios. Asigna el array a la variable "a"
 # Desafío: hay al menos tres maneras fáciles que usan numpy para generar arrays aleatorios. ¿Cuántas formas puedes encontrar?
 
-#[tu código aquí]
+#Forma 1 randint
+a = np.random.randint(0, 10, (2, 3, 5))
+
+#Forma 2 random
+#a = np.random.rand(2, 3, 5)
+
+#Forma 3 random_sample
+#a = np.random.random_sample((2, 3, 5))
 
 #4. Imprime a.
+print(a)
 
-#[tu código aquí]
+
 #5. Crea un array tridimensional de 5x2x3 con todos los valores igual a 1.
 #Asigna el array a la variable "b"
-
-#[tu código aquí]
+b = np.ones((5, 2, 3))
 
 #6. Imprime b.
 
-#[tu código aquí]
+print(b)
 
 #7. ¿Tienen a y b el mismo tamaño? ¿Cómo lo demuestras en código Python?
 
-#[tu código aquí]
+if a.shape == b.shape:
+    print('la forma es igual')
+else:
+    print('la forma no es igual')
 
 #8. ¿Es posible sumar a y b? ¿Por qué sí o por qué no?
 
-#[tu código aquí]
-
+#No se podrán sumar porque no tienen la misma forma
+try:
+    z = a + b
+    print("Suma exitosa. Resultado:\n", z)
+except ValueError as e:
+    print("Error en la suma:", e)
 
 #9. Transpone b para que tenga la misma estructura que a (es decir, se convierta en un array de 2x3x5). Asigna el array transpuesto a la variable "c".
 
-#[tu código aquí]
+c = b.transpose(1, 2, 0)
 
 #10. Intenta sumar a y c. Ahora debería funcionar. Asigna la suma a la variable "d". Pero, ¿por qué funciona ahora?
 
-#[tu código aquí]
+try:
+    d = a + c
+    print("Suma exitosa. Resultado:\n", d)
+except ValueError as e:
+    print("Error en la suma:", e)
+
+#Ahora funciona porque tiene la misma forma
 
 #11. Imprime a y d. ¿Notas la diferencia y la relación entre los dos arrays en términos de los valores? Explica.
 
-#[tu código aquí]
-
+print(a)
+print(d)
+# d es 'a' a la que se le suma 1 en cada elemento de la matriz 
 
 #12. Multiplica a y c. Asigna el resultado a e.
 
-#[tu código aquí]
+e = a * c 
 
 
 #13. ¿Es e igual a a? ¿Por qué sí o por qué no?
 
-#[tu código aquí]
-
-
+if a.all()== e.all():
+    print('Es igual e y a porque multiplicar una matriz de unos a otra matriz es como no hacer nada')
 
 #14. Identifica los valores máximos, mínimos y medios en d. Asigna esos valores a las variables "d_max", "d_min" y "d_mean"
 
-#[tu código aquí]
-
+d_max = d.max()
+d_min = d.min()
+d_mean = d.mean()
 
 #15. Ahora queremos etiquetar los valores en d. Primero crea un array vacío "f" con la misma forma (es decir, 2x3x5) que d usando `np.empty`.
 
-#[tu código aquí]
+f = np.empty((2,3,5))
 
 
 """
@@ -78,7 +98,23 @@ Al final, f debería tener solo los siguientes valores: 0, 25, 50, 75 y 100.
 Nota: no necesitas usar Numpy en esta pregunta.
 """
 
-#[tu código aquí]
+for i in range(d.shape[0]): #profundidad
+    for j in range(d.shape[1]): #filas
+        for k in range(d.shape[2]): #columnas
+            # Obtener el valor correspondiente de d
+            valor = d[i, j, k]
+
+            # Asignar valores a f
+            if valor == d_min:
+                f[i, j, k] = 0
+            elif valor == d_max:
+                f[i, j, k] = 100
+            elif valor == d_mean:
+                f[i, j, k] = 50
+            elif d_min < valor < d_mean:
+                f[i, j, k] = 25
+            elif d_mean < valor < d_max:
+                f[i, j, k] = 75
 
 
 
@@ -104,7 +140,9 @@ array([[[ 75.,  75.,  75.,  25.,  75.],
         [ 25.,  75.,   0.,  75.,  75.]]])
 """
 
-#[tu código aquí]
+print(f'esta es la matriz d {d}')
+
+print(f'Esta es la matriz f {f}')
 
 
 
@@ -120,5 +158,23 @@ array([[[ 'D',  'D',  'D',  'B',  'D'],
         [ 'B',  'D',   'A',  'D', 'D']]])
 De nuevo, no necesitas Numpy en esta pregunta.
 """
+f = np.empty((2,3,5), dtype=str)
+for i in range(d.shape[0]): #profundidad
+    for j in range(d.shape[1]): #filas
+        for k in range(d.shape[2]): #columnas
+            # Obtener el valor correspondiente de d
+            valor = d[i, j, k]
 
-#[tu código aquí]
+            # Asignar valores a f
+            if valor == d_min:
+                f[i, j, k] = 'A'
+            elif valor == d_max:
+                f[i, j, k] = 'B'
+            elif valor == d_mean:
+                f[i, j, k] = 'C'
+            elif d_min < valor < d_mean:
+                f[i, j, k] = 'D'
+            elif d_mean < valor < d_max:
+                f[i, j, k] = 'E'
+
+print(f)
